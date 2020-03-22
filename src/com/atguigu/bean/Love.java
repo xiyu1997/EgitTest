fea1181518d48a7be71a3aa0d121593dd4750d91
xiyu1997 <992266900@qq.com>
2020-03-22 15:17:23 +0800

    1111

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -3,7 +3,7 @@ package com.atguigu.bean;
 public class Love {
 
 	public static void main(String[] args) {
-		System.out.println("ILoveYou");
+		System.out.println("ILoveYou1111");
 		System.out.println("ILoveYou,too");
 	}
 }
